18061b7fc8a449918543f1553107be8aeb6a7beb
RickyYu <yuyiuwing@gmail.com>
2022-05-28 01:40:17 +0800

    setup environment and install dependencies of client, server and db

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit:"30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit:"30mb", extended: true }));
+app.use(cors());
+
+// https://www.mongodb.com/cloud/atlas
+const CONNECTION_URL = 'mongodb+srv://yuyiuwing:22579450@cluster0.pvprcmv.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+// { useNewUrlParser: true, useUnifiedTopology: true } input object: to make sure we don't get any warnings in the console
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+.catch((error) => console.log(error.message));
+
+// to make sure we don't get any warnings in the console
+// mongoose.set('useFindAndModify', false);
